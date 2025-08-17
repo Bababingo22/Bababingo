46ee53959bb5752951ba7fdf9b9837472b2b04f8
Bababingo22 <bababingo22@gmail.com>
2025-08-17 17:07:48 +0300

    Update asgi.py

--- backend/yaba_bingo/asgi.py
+++ backend/yaba_bingo/asgi.py
@@ -2,12 +2,16 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from bingo import routing as bingo_routing
 
+# 1. Configure Django's settings FIRST.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yaba_bingo.settings")
 django.setup()
 
+# 2. NOW it is safe to import your application's code.
+from channels.auth import AuthMiddlewareStack
+from bingo import routing as bingo_routing
+
+# 3. Define the application.
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
